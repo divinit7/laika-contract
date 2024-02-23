f5348866798c1477337cfd33a79771ba34caced1
harshcrop <harshcrop@gmail.com>
2024-02-23 12:24:36 +0530

    change deployment script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,13 +1,23 @@
-import { ethers } from "hardhat";
+import hre from "hardhat";
 
 async function main() {
-  const ContractFactory = await ethers.getContractFactory("LaikaAI");
+  let initialOwner = "0xfEceA7b046b4DaFACE340c7A2fe924cf41b6d274"
 
-  // TODO: Set addresses for the contract arguments below
+  const ContractFactory = await hre.ethers.getContractFactory("LaikaAI");
   const instance = await ContractFactory.deploy(initialOwner);
   await instance.waitForDeployment();
+  let contractAddress = await instance.getAddress()
+  console.log("LaikaAI contract deployed at: ", contractAddress);
 
-  console.log(`Contract deployed to ${await instance.getAddress()}`);
+  // sleep for 5 seconds
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+
+  // verify contract
+  console.log("Verify contract process...")
+  await hre.run("verify:verify", {
+    address: contractAddress,
+    constructorArguments: [initialOwner],
+  });
 }
 
 // We recommend this pattern to be able to use async/await everywhere
