4b1660f88bb0e463733e42dcb4befd74010b9cdd
harshcrop <harshcrop@gmail.com>
2024-02-23 12:25:09 +0530

    change hardhat config & added example env file

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,15 +1,38 @@
-import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import dotenv from 'dotenv';
+dotenv.config();
 
+const defaultKey = "0000000000000000000000000000000000000000000000000000000000000000";
 
-const config: HardhatUserConfig = {
+const config = {
   solidity: {
     version: "0.8.20",
     settings: {
       optimizer: {
-        enabled: true,
+        "enabled": true,
+        "runs":  200,
       },
+      "evmVersion": "istanbul",
     },
+  },
+  etherscan: {
+    apiKey: {
+      polygonMumbai: process.env.MUMBAI_SCAN_API_KEY,
+      polygon: process.env.MAINNET_SCAN_API_KEY
+    }
+  },
+  networks: {
+    mumbai: {
+      url: process.env.MUMBAI_RPC_URL || '',
+      accounts: [process.env.ACCOUNT_PRIVATE_KEY || defaultKey]
+    },
+    polygon:{
+      url: process.env.POLYGON_MAINNET_RPC_URL || '',
+      accounts: [process.env.MAINNET_ACCOUNT_PRIVATE_KEY || defaultKey]
+    },
+    hardhat: {
+      chainId: 421613 
+    }
   },
 };
 
