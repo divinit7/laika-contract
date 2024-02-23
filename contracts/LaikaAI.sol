430cfbe1d985a2bf7f0598f2d6145b2747987336
harshcrop <harshcrop@gmail.com>
2024-02-23 12:24:21 +0530

    Improve the NatSpecs

--- contracts/LaikaAI.sol
+++ contracts/LaikaAI.sol
@@ -7,17 +7,29 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+/**
+ * @title LaikaAI
+ * @dev Extends ERC20, ERC20Burnable, ERC20Permit, ERC20Votes, and Ownable contracts from OpenZeppelin.
+ */
 contract LaikaAI is ERC20, ERC20Burnable, ERC20Permit, ERC20Votes, Ownable {
+    /**
+     * @dev Constructor that initializes the LaikaAI token with an initial supply and transfers it to the deployer.
+     * @param initialOwner The address that will become the initial owner of all LaikaAI tokens.
+     */
     constructor(address initialOwner)
-        ERC20("Laika AI ", "LKI")
-        ERC20Permit("Laika AI ")
+        ERC20("Laika AI", "LKI")
+        ERC20Permit("Laika AI")
         Ownable(initialOwner)
     {
         _mint(msg.sender, 1000000000 * 10 ** decimals());
     }
 
-    // The following functions are overrides required by Solidity.
-
+    /**
+     * @dev Overrides the _update function to ensure compatibility with ERC20Votes.
+     * @param from The address from which tokens are transferred.
+     * @param to The address to which tokens are transferred.
+     * @param value The amount of tokens transferred.
+     */
     function _update(address from, address to, uint256 value)
         internal
         override(ERC20, ERC20Votes)
@@ -25,6 +37,11 @@ contract LaikaAI is ERC20, ERC20Burnable, ERC20Permit, ERC20Votes, Ownable {
         super._update(from, to, value);
     }
 
+    /**
+     * @dev Overrides the nonces function to return the nonce for permit signature verification.
+     * @param owner The address for which the nonce is queried.
+     * @return The current nonce associated with the owner address.
+     */
     function nonces(address owner)
         public
         view
