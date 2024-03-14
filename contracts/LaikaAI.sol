cacf91d79ff4b9834b179379cfa81e9b77c3033b
harshcrop <harshcrop@gmail.com>
2024-03-14 13:16:46 +0530

    solidity push0 issue solve, style guide issue fix &  Large numeric literals readable issue fix

--- contracts/LaikaAI.sol
+++ contracts/LaikaAI.sol
@@ -1,27 +1,27 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+import '@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol';
+import '@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol';
+import '@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
 
 /**
  * @title LaikaAI
  * @dev Extends ERC20, ERC20Burnable, ERC20Permit, ERC20Votes, and Ownable contracts from OpenZeppelin.
  */
 contract LaikaAI is ERC20, ERC20Burnable, ERC20Permit, ERC20Votes, Ownable {
+    uint256 private constant INITIAL_SUPPLY = 1_000_000_000 * 10 ** decimals();
+
     /**
      * @dev Constructor that initializes the LaikaAI token with an initial supply and transfers it to the deployer.
      * @param initialOwner The address that will become the initial owner of all LaikaAI tokens.
      */
-    constructor(address initialOwner)
-        ERC20("Laika AI", "LKI")
-        ERC20Permit("Laika AI")
-        Ownable(initialOwner)
-    {
-        _mint(msg.sender, 1000000000 * 10 ** decimals());
+    constructor(
+        address initialOwner
+    ) ERC20('Laika AI', 'LKI') ERC20Permit('Laika AI') Ownable(initialOwner) {
+        _mint(msg.sender, INITIAL_SUPPLY);
     }
 
     /**
@@ -30,10 +30,11 @@ contract LaikaAI is ERC20, ERC20Burnable, ERC20Permit, ERC20Votes, Ownable {
      * @param to The address to which tokens are transferred.
      * @param value The amount of tokens transferred.
      */
-    function _update(address from, address to, uint256 value)
-        internal
-        override(ERC20, ERC20Votes)
-    {
+    function _update(
+        address from,
+        address to,
+        uint256 value
+    ) internal override(ERC20, ERC20Votes) {
         super._update(from, to, value);
     }
 
@@ -42,12 +43,9 @@ contract LaikaAI is ERC20, ERC20Burnable, ERC20Permit, ERC20Votes, Ownable {
      * @param owner The address for which the nonce is queried.
      * @return The current nonce associated with the owner address.
      */
-    function nonces(address owner)
-        public
-        view
-        override(ERC20Permit, Nonces)
-        returns (uint256)
-    {
+    function nonces(
+        address owner
+    ) public view override(ERC20Permit, Nonces) returns (uint256) {
         return super.nonces(owner);
     }
 }
